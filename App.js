d7a5c70db0f8be458e77f274da3fd7de3f8508dd
Rishi <sangarerishi@gmail.com>
2023-05-13 11:24:14 +0530

    Created Category Section

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput} from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, ScrollView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const foodCategories = ['Italian', 'Mexican', 'Indian', 'Chinese', 'Thai', 'Japanese'];
+
   return (
     <View style={styles.container}>
-    <View style={styles.header}>
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Search for food"
-      />
-      <View style={styles.profileIcon} />
+      <View style={styles.header}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search for food"
+        />
+        <View style={styles.profileIcon} />
+      </View>
+      <View style={styles.dailyMealsSection}>
+        <Text style={styles.sectionTitle}>Daily Meals</Text>
+        {/* Add your daily meals content here */}
+      </View>
+      <View style={styles.foodCategoriesSection}>
+        <Text style={styles.sectionTitle}>Food Categories</Text>
+        <View style={styles.fullCategory}>
+        <View style={styles.scrollLeft}>
+        <ScrollView  verticle showsVerticalScrollIndicator={false}>
+          {foodCategories.map((category, index) => (
+            <View key={index} style={styles.categoryItem}>
+              <Text>{category}</Text>
+            </View>
+          ))}
+        </ScrollView>
+        </View>
+        <View style={styles.scrollRight}>
+        <ScrollView >
+              <Text>Hi These are list of Items</Text>
+        </ScrollView>
+        </View>
+        </View>
+      </View>
     </View>
-    <View style={styles.dailyMealsSection}>
-      <Text style={styles.sectionTitle}>Daily Meals</Text>
-      {/* Add your daily meals content here */}
-    </View>
-    <View style={styles.foodCategoriesSection}>
-      <Text style={styles.sectionTitle}>Food Categories</Text>
-      {/* Add your food categories content here */}
-    </View>
-  </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -29,7 +46,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   header: {
-    flex:1,
+    
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -38,13 +55,12 @@ const styles = StyleSheet.create({
     paddingBottom: 12,
   },
   searchBar: {
-    flex: 1,
+    flex: 1,  
     backgroundColor: '#f5f5f5',
     borderRadius: 8,
     paddingHorizontal: 16,
     height: 40,
     marginRight: 12,
-    paddingTop:10,
   },
   profileIcon: {
     width: 40,
@@ -53,18 +69,45 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
   },
   dailyMealsSection: {
-    flex:3,
+    flex:1,
     paddingVertical: 16,
     paddingHorizontal: 16,
   },
   foodCategoriesSection: {
-    flex:8,
-    paddingVertical: 16,
-    paddingHorizontal: 16,
+    flex:4,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    width:155
   },
   sectionTitle: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 12,
   },
+  categoryItem: {
+    backgroundColor: '#f5f5f5',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginRight: 8,
+    textAlign:'center',
+    margin:12,
+    width:75,
+    height:60
+  },
+  scrollLeft:{
+    flex:7,
+    borderRightWidth:1
+  },
+  scrollRight:{
+    flex:3
+    
+  },
+  fullCategory:{
+    flex:1,
+    flexDirection:'row',
+  }
+
 });
+
+export default App;
